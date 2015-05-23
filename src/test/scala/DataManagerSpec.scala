c9b011d845e4752390b0910fe9708e205b5f7b74
bencardinal <ben.cardinal@gmail.com>
2015-05-23 15:52:59 -0500

    Added first test! Hooray!

--- src/test/scala/DataManagerSpec.scala
+++ src/test/scala/DataManagerSpec.scala
@@ -1,5 +1,11 @@
 import org.scalatest._
 
-class DataManagerSpec {
+class DataManagerSpec extends FlatSpec with Matchers {
 
+   "The PrimesDataManager" should "throw IllegalArgumentException if max value is greater than constructor maximum" in {
+      val dm = new PrimesDataManager(10)
+      a [IllegalArgumentException] should be thrownBy {
+         dm.GetPrimes(0, 11)
+      }
+   }
 }
