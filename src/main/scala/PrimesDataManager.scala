4f2208ca27f93f64c7a2bc3d55314d73b87818d0
bencardinal <ben.cardinal@gmail.com>
2015-05-23 21:05:26 -0500

    Non database implementation using Iterator.

--- src/main/scala/PrimesDataManager.scala
+++ src/main/scala/PrimesDataManager.scala
@@ -2,6 +2,20 @@ class PrimesDataManager(maxValue: Int) {
    val _maxValue = maxValue
    // TODO Redis magic here in the constructor to build an internal list of integers.
    
+   // Use iterator to iterate values from whatever maximum is currently in the
+   // Redis database up to the provided maxVal.  Use a filtered query from
+   // the databse to get a list of already calculated primes whose square is
+   // less than the next potential prime.
+   // Once a prime is found, push it to the database.
+   val primeArray = new scala.collection.mutable.ArrayBuffer[Int]()
+   val primeIter = Iterator.from(2).filter(i => primeArray.takeWhile(j => j * j <= i).forall(k => i %k > 0))
+   var currentPrime = 2
+   while (currentPrime <= maxValue)
+   {
+      currentPrime = primeIter.next
+      primeArray.append(currentPrime)
+   }
+   
    def GetPrimes(lowerLimit: Int, upperLimit: Int): Array[Int] = {
              
       if (upperLimit > _maxValue) {
@@ -24,7 +38,16 @@ class PrimesDataManager(maxValue: Int) {
       // TODO stub
       buf.append(lowerLimit)
       buf.append(upperLimit)
-       
-      return buf.toArray
+
+      // Use a stream to build the values as needed and then write them to
+      // the Redis database.
+      lazy val primes: Stream[Int] = 
+         2 #:: Stream.from(3, 2)
+         .filter(i => primes.takeWhile(j => j * j <= i)
+               .forall(k => i % k > 0)) // filter those that are not divisible by earlier calculated prime
+      
+      
+      //return primes.dropWhile(p => p <= lowerLimit).takeWhile(p => p <= upperLimit)
+      return primeArray.dropWhile(p => p <= lowerLimit).takeWhile(p => p <= upperLimit).toArray
    }   
 }
