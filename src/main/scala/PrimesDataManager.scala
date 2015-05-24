7b75a10f483b973ed2b18261c44a2bec36c34713
bencardinal <ben.cardinal@gmail.com>
2015-05-24 17:56:45 -0500

    Mid kludge, cleanup in next commit

--- src/main/scala/PrimesDataManager.scala
+++ src/main/scala/PrimesDataManager.scala
@@ -1,6 +1,6 @@
 import com.redis._
 
-class PrimesDataManager(maxValue: Long, redisHost: String, redisPort: Int, deleteKey: Boolean) {  
+class PrimesDataManager(maxValue: Int, redisHost: String, redisPort: Int, deleteKey: Boolean) {  
    val dbKey = "PrimesDataManager:primeArray";
    val _maxValue = maxValue
    // TODO Redis magic here in the constructor to build an internal list of integers.
@@ -12,10 +12,10 @@ class PrimesDataManager(maxValue: Long, redisHost: String, redisPort: Int, delet
    // Once a prime is found, push it to the database.
 
    // primeArray will hold the local copy of the primes.
-   //val primeArray = new scala.collection.mutable.ArrayBuffer[Int]()
+   val primeArray = new scala.collection.mutable.ArrayBuffer[Int]()
    // Default to the second prime, 3, since the database will be initialized with
    // the first prime, 2.
-   var currentPrime = 2
+   var currentPrime  = 1
 
    val client = new RedisClient(redisHost, redisPort)
    
@@ -29,44 +29,48 @@ class PrimesDataManager(maxValue: Long, redisHost: String, redisPort: Int, delet
          // contains only a sequential set of valid prime numbers starting
          // with 2.  Things will go badly if this is not the case!
          val count: Long = client.llen(dbKey).getOrElse(0)
-         val dbList = client.lrange(dbKey, 0, count.toInt).getOrElse(List())
-         //dbList.foreach(a => primeArray.append(a.get.toInt))
-         currentPrime = dbList.last.get.toInt
+         primeArray.appendAll(client.lrange(dbKey, 0, count.toInt).getOrElse(List()).map(p => p.get.toInt))
+         //dbList.foreach(a => primeArray.append(a.get.toLong))
+         currentPrime = primeArray.last
       } else {
          // This is MY key and someone has messed with it so I'll show them!
          client.del(dbKey)
-         client.lpush(dbKey, 2)
       }
    }
-   else  
-   { 
-      client.lpush(dbKey, 2)
-   }
 
-   val dbScanner: Stream[Long] = Stream.from(0).map(i => client.lindex(dbKey, i).getOrElse("-1").toLong)
+   //val dbScanner: Stream[Long] = Stream.from(0).map(i => client.lindex(dbKey, i).getOrElse("-1").toLong)
    
    //var largestPrime = currentPrime
-   val primeIter = Iterator.from(currentPrime) // Start from the current prime (may be >2 if loaded from database)
-   .filter(i => dbScanner                      // Filter based on the current contents of the primeArray ...
-         .takeWhile(j => j * j <= i)           // ... and take values whose square is less than the current value
-         .forall(k => i % k > 0))              // Yield the value if it is not evenly divisible by any previous prime
+   val primeIter = Iterator.from(currentPrime + 1) // Start one after the current prime (may be >2 if loaded from database)
+   .filter(i => primeArray                         // Filter based on the current contents of the primeArray ...
+         .takeWhile(j => j * j <= i)               // ... and take values whose square is less than the current value
+         .forall(k => i % k > 0))                  // Yield the value if it is not evenly divisible by any previous prime
    
-   primeIter.next();
+//   while (currentPrime <= maxValue)
+//   {
+//      println("currentPrime: " + currentPrime)
+//      currentPrime = primeIter.next
+//      primeArray.append(currentPrime)
+//      client.rpush(dbKey, currentPrime)
+//   }
+   
+   //if (primeArray.length > 0) primeIter.next() //KLUDGE!
    
    primeIter.takeWhile(p => p <= maxValue)     // Iterator will return as long as the prime is less or equal to max
-   .foreach(p => {                             
-      //primeArray.append(p)                   // Append to local array
+   .foreach(p => {   
+      //println("currentPrime "+p)
+      primeArray.append(p)                   // Append to local array
       client.rpush(dbKey, p)                   // Push to databse
       //largestPrime = p                         // Update largestPrime
       //r.rpush(dbKey, p)                        // Push to database
       })
 
-    dbScanner.dropWhile(p => p > 0)
+    //dbScanner.dropWhile(p => p > 0)
     
    /*
     * Get an array of prime numbers between lowerLimit and upperLimit inclusive
     */
-   def GetPrimes(lowerLimit: Long, upperLimit: Long): Array[Long] = {
+   def GetPrimes(lowerLimit: Long, upperLimit: Long): Array[Int] = {
              
       if (upperLimit > _maxValue) {
          throw new IllegalArgumentException(
@@ -95,9 +99,9 @@ class PrimesDataManager(maxValue: Long, redisHost: String, redisPort: Int, delet
       //val count: Long = client.llen(dbKey).getOrElse(0)
       //val dbList = client.lrange(dbKey, 0, count.toInt).getOrElse(List())
       //dbList.dropWhile(p => p.get.toInt < lowerLimit).takeWhile(p => p.get.toInt <= upperLimit).map(p => p.get.toInt).toArray
-      //primeArray.dropWhile(p => p < lowerLimit).takeWhile(p => p <= upperLimit).toArray
-      //val dbScanner: Iterator[Int] = Iterator.from(0).map(i => client.lindex(dbKey, i).get.toInt)
-      dbScanner.dropWhile(p => p < lowerLimit).takeWhile(p => p <= upperLimit && p > 0).toArray
+      primeArray.dropWhile(p => p < lowerLimit).takeWhile(p => p <= upperLimit).toArray
+      //val dbScanner: Iterator[Long] = Iterator.from(0).map(i => client.lindex(dbKey, i).get.toInt)
+      //dbScanner.dropWhile(p => p < lowerLimit).takeWhile(p => p <= upperLimit && p > 0).toArray
 
    }   
 }
--- src/test/scala/DataManagerSpec.scala
+++ src/test/scala/DataManagerSpec.scala
@@ -1,17 +1,23 @@
 import org.scalatest._
 
 class DataManagerSpec extends FlatSpec with Matchers {
-   val upperBound = 1000000;
+   val upperBound = 100;
 
    "The PrimesDataManager constructor" should "throw RuntimeException if redis server cannot be found" in {
       a [RuntimeException] should be thrownBy {
-         var badDm = new PrimesDataManager(upperBound, "badhost", 6379, false)
+         var badDm = new PrimesDataManager(100, "badhost", 6379, false)
       }
    }
-   val t0 = java.lang.System.currentTimeMillis()
+   
+   "The PrimesDataManager constructor" should "only add each prime once when loading from cached values" in {
+      val dm1 = new PrimesDataManager(10, "localhost", 6379, true)  // Clear database cache
+      val dm2 = new PrimesDataManager(20, "localhost", 6379, false) // Reuse database cache (up to 10)
+      val primes = dm2.GetPrimes(0, 20)
+      primes should contain only (2, 3, 5, 7, 11, 13, 17, 19)
+      primes should have length 8
+   }
+
    val dm = new PrimesDataManager(upperBound, "localhost", 6379, false)
-   val t1 = java.lang.System.currentTimeMillis()
-   info("Constructor took: "+(t1-t0)+" msec")
    
    "The PrimesDataManager" should "throw IllegalArgumentException if upperLimit is greater than constructor maximum" in {
       a [IllegalArgumentException] should be thrownBy {
@@ -50,7 +56,15 @@ class DataManagerSpec extends FlatSpec with Matchers {
    }
 
    "The PrimesDataManager" should "return 79,498 elements between 0 and 1,000,000" in {
-      val primes = dm.GetPrimes(0, 1000000)
+      val t0 = java.lang.System.currentTimeMillis()
+      val bigDm = new PrimesDataManager(1000000, "localhost", 6379, true)
+      val t1 = java.lang.System.currentTimeMillis()
+      info("Building new set of primes < 1000000 took: "+(t1-t0)+" msec")
+      val t2 = java.lang.System.currentTimeMillis()
+      val bigDmCached = new PrimesDataManager(1000000, "localhost", 6379, false)
+      val t3 = java.lang.System.currentTimeMillis()
+      info("Loading cached primes < 1000000 took: "+(t3-t2)+" msec")
+      val primes = bigDm.GetPrimes(0, 1000000)
       primes should have length 78498
    }
 }
