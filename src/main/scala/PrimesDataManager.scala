48467237cda97a9a38f10465c3317969de487456
bencardinal <ben.cardinal@gmail.com>
2015-05-25 21:36:37 -0500

    Renamed a couple variables for consistency

--- src/main/scala/PrimesDataManager.scala
+++ src/main/scala/PrimesDataManager.scala
@@ -21,47 +21,47 @@ class PrimesDataManager(maxValue: Int, redisHost: String, redisPort: Int, delete
    
    if (maxValue < 2) throw new IllegalArgumentException("maxValue must be greater than or equal to 2")   
 
-   val dbKey = PrimesDataManagerParameters.dbKey;                   // Key for stored primes
+   val _dbKey = PrimesDataManagerParameters.dbKey;                   // Key for stored primes
    val _maxValue = maxValue                                         // Keep max value for error checking
-   val primeArray = new scala.collection.mutable.ArrayBuffer[Int]() // Local copy of primes
+   val _primeArray = new scala.collection.mutable.ArrayBuffer[Int]() // Local copy of primes
 
    // Will throw an ConnectException redis server is not available
    val dbClient = new RedisClient(redisHost, redisPort)
    
    if (  !deleteKey
-      && dbClient.exists(dbKey)
-      && dbClient.getType(dbKey).getOrElse("").equals("list")
+      && dbClient.exists(_dbKey)
+      && dbClient.getType(_dbKey).getOrElse("").equals("list")
       ) {
          // There is an assumption here that the list of data in this key
          // contains only a sequential set of valid prime numbers.  If there
          // is a bunch of bad data then things will go badly.  Validating all
          // the cached values would negate the benefit of loading them.
-         val count: Long = dbClient.llen(dbKey).getOrElse(0)         
-         primeArray.appendAll(
-               dbClient.lrange(dbKey, 0, count.toInt) // Get entire list
+         val count: Long = dbClient.llen(_dbKey).getOrElse(0)         
+         _primeArray.appendAll(
+               dbClient.lrange(_dbKey, 0, count.toInt) // Get entire list
                .getOrElse(List())                     // Return an empty list if that fails
                .map(p => p.get.toInt))                // And map Option to an integer.
    }
    
-   if (primeArray.length < 2) {
+   if (_primeArray.length < 2) {
       // The array should contain at least 2 values (2, 3) so if it doesn't
       // then reset the initial state and load with 2 and 3
-      primeArray.clear()
-      dbClient.del(dbKey)
-      primeArray.append(2, 3)
-      dbClient.rpush(dbKey, 2, 3)
+      _primeArray.clear()
+      dbClient.del(_dbKey)
+      _primeArray.append(2, 3)
+      dbClient.rpush(_dbKey, 2, 3)
    }
 
    val primeIter = 
-      Iterator.from(primeArray.last + 2, 2) // Start two after the current prime (may be >5 if loaded from database)
-      .filter(i => primeArray               // Filter based on the current contents of the primeArray ...
+      Iterator.from(_primeArray.last + 2, 2) // Start two after the current prime (may be >5 if loaded from database)
+      .filter(i => _primeArray               // Filter based on the current contents of the primeArray ...
             .takeWhile(j => j * j <= i)     // ... and take values whose square is less than the current value
             .forall(k => i % k > 0))        // Yield the value if it is not evenly divisible by any previous prime
    
    primeIter.takeWhile(p => p <= maxValue)  // Take values as long as the prime is less or equal to max
    .foreach(p => {                          // For each prime from the iterator ...
-      primeArray.append(p)                  // ... Append to local array
-      dbClient.rpush(dbKey, p)              // ... Push to databse
+      _primeArray.append(p)                  // ... Append to local array
+      dbClient.rpush(_dbKey, p)              // ... Push to databse
    })
    
    dbClient.disconnect
@@ -90,6 +90,6 @@ class PrimesDataManager(maxValue: Int, redisHost: String, redisPort: Int, delete
                "lowerLimit must be less than or equal to upperLimit")                
       }
 
-      primeArray.dropWhile(p => p < lowerLimit).takeWhile(p => p <= upperLimit).toArray
+      _primeArray.dropWhile(p => p < lowerLimit).takeWhile(p => p <= upperLimit).toArray
    }   
 }
